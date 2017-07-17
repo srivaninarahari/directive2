c01dedb7881ea4e01548b048d9170c6a3ca5d5f5
srivaninarahari <srivaninarahari18@gmail.com>
2017-07-17 16:15:57 +0530

    sree

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-
+import { TableComponent } from './table/table.component';
+import { TableDirective } from 'app/table.directive';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TableComponent,
+    TableDirective
   ],
   imports: [
     BrowserModule
---
+++ src/app/table.directive.ts
@@ -0,0 +1,16 @@
+import{Directive,Input,HostBinding,HostListener} from '@angular/core';
+@Directive({
+    selector:'[myTable]',
+
+})
+
+export class TableDirective{
+    
+  
+    constructor( )
+    {
+
+
+    }
+
+}
